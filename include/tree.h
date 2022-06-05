7a6a8011699e67566c3a427262c9f93ee9bc81b9
artem <99349934+ArtemPr1@users.noreply.github.com>
2022-06-05 19:29:08 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,59 +1,56 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
-#include <algorithm>
-#include <string>
+
 #include <vector>
-using std::string;
-using std::vector;
-class tree {
+#include <string>
+class Tree {
  private:
-    struct Node {
-        char value;
-        vector<Node*> iter;
-    };
-    Node* root;
-    vector<string> iter2;
-    void createtree(Node* root, vector<char> ch) {
-        if (!ch.size()) {
-            return;
-        }
-        if (root->value != '*') {
-            ch.erase(find(ch.begin(), ch.end(), root->value));
-        }
-        for (size_t i = 0; i < ch.size(); i++)
-            root->iter.push_back(new Node());
-        for (size_t i = 0; i < root->iter.size(); i++) {
-            root->iter[i]->value = ch[i];
-            createtree(root->iter[i], ch);
+  struct Node {
+  char smthVal;
+  std::vector<Node*> greList;
+  };
+  Node* root;
+  void addTree(Node* root, std::vector<char> per) {
+    if (!per.size())
+      return;
+    if (root->smthVal != '*') {
+      for (auto k = per.begin(); k != per.end(); k++)
+        if (*k == root->smthVal) {
+          per.erase(k);
+          break;
         }
     }
-    void Permut(Node* root, string str = "") {
-        if (!root->iter.size()) {
-            str += root->value;
-            iter2.push_back(str);
-            return;
-        }
-        if (root->value != '*') {
-            str += root->value;
-        }
-        for (int i = 0; i < root->iter.size(); i++) {
-            Permut(root->iter[i], str);
-        }
+    for (size_t i = 0; i < per.size(); i++)
+      root->greList.push_back(new Node());
+    for (size_t i = 0; i < root->greList.size(); i++)
+      root->greList[i]->smthVal = per[i];
+    for (size_t i = 0; i < root->greList.size(); i++)
+      addTree(root->greList[i], per);
+  }
+  std::vector<std::string> permutation;
+  void prm(Node* root, std::string symbol = "") {
+    if (!root->greList.size()) {
+      symbol += root->smthVal;
+      permutation.push_back(symbol);
     }
+    if (root->smthVal != '*')
+      symbol += root->smthVal;
+    for (size_t i = 0; i < root->greList.size(); i++)
+      prm(root->greList[i], symbol);
+  }
+
 
  public:
-    string operator[](int i) const {
-        if (i >= iter2.size()) {
-            return "";
-        }
-        return iter2[i];
+    std::string operator[](int i) const {
+      if (i >= permutation.size())
+        return "";
+      return permutation[i];
     }
-    explicit tree(vector<char> value) {
-        root = new Node();
-        root->value = '*';
-        createtree(root, value);
-        Permut(root);
+      explicit Tree(std::vector<char> smthVal) {
+      root = new Node();
+      root->smthVal = '*';
+      addTree(root, smthVal);
+      prm(root);
     }
 };
 #endif  // INCLUDE_TREE_H_
