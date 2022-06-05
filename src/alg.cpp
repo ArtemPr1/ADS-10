6273b42b472e5badb125e3b31e3a02bc134ae0af
artem <99349934+ArtemPr1@users.noreply.github.com>
2022-06-05 19:32:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,12 +5,11 @@
 #include  <cstdlib>
 #include  "tree.h"
 std::vector<char> getPerm(const Tree& tree, int n) {
-  // напишите реализацию
   std::string genTree = tree[n - 1];
   std::vector<char> permutation;
   for (int i = 0; i < genTree.length(); i++)
-  {
-    permutation.push_back(genTree[i]);
-  }
+    {
+      permutation.push_back(genTree[i]);
+    }
   return permutation;
 }
